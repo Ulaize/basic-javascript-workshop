8fc6da505eb78f6123253dbb98c0801e36c65975
Ulaize <ulaize@creative-tweed.com>
2015-09-11 18:18:08 +0000

    Function to get first character of a string

---
+++ answers.js
@@ -0,0 +1,14 @@
+//Javascript basics
+
+//Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+
+function firstCharacter (string) {
+    return string[0];
+}
+
+
+firstCharacter("Hello");
+
+firstCharacter("Ulaize");
+
+firstCharacter("");
