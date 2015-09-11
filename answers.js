34406a11d031ba69ccf64144ebd9af5cf37b9bb6
Ulaize <ulaize@creative-tweed.com>
2015-09-11 18:23:43 +0000

    Added a function that returns the last character of a string

--- answers.js
+++ answers.js
@@ -1,6 +1,6 @@
 //Javascript basics
 
-//Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+//Function that returns the first character of a string
 
 function firstCharacter (string) {
     return string[0];
@@ -12,3 +12,15 @@ firstCharacter("Hello");
 firstCharacter("Ulaize");
 
 firstCharacter("");
+
+//Function that returns the last character of a string
+
+function lastCharacter (string) {
+    return string[string.length-1]
+}
+
+lastCharacter("Hello");
+
+lastCharacter("Ulaize");
+
+lastCharacter("");
