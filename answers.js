f0012d9b0f2316ae6153e3106e6fd7a099d26a25
Ulaize <ulaize@creative-tweed.com>
2015-09-14 18:03:10 +0000

    Function that sorts a bunch of numbers in decendent order and returns the largest number.

--- answers.js
+++ answers.js
@@ -195,3 +195,35 @@ function capitalize( string )
 }
 
 capitalize("what is up?");
+
+
+
+//Function that sorts a bunch of numbers in decendent order and returns the largest number.
+
+
+var someNum = [1,2,3,4,8,2,5,9,2,10,22];
+
+someNum.sort(function(a, b){return b-a});
+
+someNum[1];
+someNum[0];
+someNum[10];
+
+
+
+//Write a function that takes an array, and returns a filtered array. The filtered array should only contain the truthy values from the initial array. Hint: there is an array method called “filter” that can help you with this :)
+
+
+
+
+
+//Write a function that takes an array of numbers, and returns the sum of all the numbers in the array.
+
+
+
+
+//Write a function that takes two arrays, and returns an array of all elements that are only in one array. For example, with [1,2,3] and [1,2,4,5] the function should return [3,4,5]. Test your function on different inputs. Hint: you should look up array methods indexOf and slice.
+
+
+
+
